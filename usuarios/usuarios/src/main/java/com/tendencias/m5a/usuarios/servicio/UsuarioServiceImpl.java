b629bb7cf2df6bb7f837a50614e5e84cdc0c3d53
Abel  Gómez <121979015+abelgomezc@users.noreply.github.com>
2023-06-01 17:50:03 -0500

    Add files via upload

---
+++ usuarios/usuarios/src/main/java/com/tendencias/m5a/usuarios/servicio/UsuarioServiceImpl.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tendencias.m5a.usuarios.servicio;
+
+
+import com.tendencias.m5a.usuarios.model.Usuario;
+import com.tendencias.m5a.usuarios.repositorio.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Abel Gomez
+ */
+@Service
+public class UsuarioServiceImpl extends GenericServiceImpl<Usuario, Integer> implements GenericService<Usuario, Integer> {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public CrudRepository<Usuario, Integer> getDao() {
+        return usuarioRepository;
+    }
+
+
+}
